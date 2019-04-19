25c99105320525bef10f1192af4c793998d5e91b
Sachin <sachin.pathirana@qualitapps.com>
2019-04-19 16:30:41 +0530

    alarm add to database

--- app/src/main/java/com/example/alarmsystem/AddAlarm.java
+++ app/src/main/java/com/example/alarmsystem/AddAlarm.java
@@ -1,5 +1,6 @@
 package com.example.alarmsystem;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,20 +8,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AddAlarm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TimePicker timePicker;
     private Spinner spinner;
     private MediaPlayer alarmTone;
+    private EditText alarmNameText;
     private static final String[] paths = {"Select Alarm Tone","Tone 1", "Tone 2", "Tone 3"};
+    DataBaseHelper mDataBaseHelper;
+    String alarmName;
+    String alarmTime;
+    int alarmToneSelected;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
         timePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmNameText = (EditText) findViewById(R.id.alarm_name_txt);
          timePicker.setIs24HourView(true);
 
         spinner = (Spinner)findViewById(R.id.tone);
@@ -30,44 +39,36 @@ public class AddAlarm extends AppCompatActivity implements AdapterView.OnItemSel
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+        mDataBaseHelper = new DataBaseHelper(this);
+        mDataBaseHelper.addAlarm("Alarm1","08:00",2);
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                alarmTime = Integer.toString(hourOfDay)+":"+Integer.toString(minute);
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Handler handler=new Handler();
+//        Handler handler=new Handler();
         if(alarmTone!=null){
             alarmTone.stop();
         }
+        alarmToneSelected = position;
         switch (position){
             case 1:
                 alarmTone = MediaPlayer.create(this, R.raw.twin_alarm);
                 alarmTone.start();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        alarmTone.stop();
-                    }
-                }, 5000);
                 break;
             case 2:
                 alarmTone = MediaPlayer.create(this,R.raw.alarm_ringtones);
                 alarmTone.start();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        alarmTone.stop();
-                    }
-                }, 5000);
                 break;
             case 3:
                 alarmTone=MediaPlayer.create(this,R.raw.wrecker_alarm);
                 alarmTone.start();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        alarmTone.stop();
-                    }
-                }, 3000);
                 break;
         }
     }
@@ -75,5 +76,24 @@ public class AddAlarm extends AppCompatActivity implements AdapterView.OnItemSel
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+    public void saveAlarm(View view) {
+        alarmName = alarmNameText.getText().toString();
+
+        if (alarmName != null && alarmTime != null && alarmToneSelected > 0) {
+            boolean insertData = mDataBaseHelper.addAlarm(alarmName, alarmTime, alarmToneSelected);
+            if(insertData){
+                toastMessage("Added Successfully");
+            }
+            else {
+                toastMessage("Error in Adding Alarm");
+            }
+        } else {
+            toastMessage("Please fill all values");
+        }
+    }
+    public void toastMessage(String text)
+    {
+        Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/alarmsystem/DataBaseHelper.java
@@ -0,0 +1,66 @@
+package com.example.alarmsystem;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = "alarm_table";
+    private static final String COL1 = "ID";
+    private static final String COL2 = "name";
+    private static final String COL3 = "time";
+    private static final String COL4 = "tone";
+
+    public DataBaseHelper( Context context) {
+        super(context, "ALARM_DB",null,2);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            String createTable = "CREATE TABLE alarm_table( ID INTEGER PRIMARY KEY AUTOINCREMENT , name VARCHAR , time VARCHAR , tone INTEGER);";
+            db.execSQL(createTable);
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sqlTable = "DROP TABLE IF EXISTS "+TABLE_NAME;
+        db.execSQL(sqlTable);
+
+        onCreate(db);
+
+    }
+
+    public boolean addAlarm(String name,String time,int tone)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,time);
+        contentValues.put(COL4,tone);
+
+        Log.d(TAG,"addData:adding"+name+"to"+TABLE_NAME);
+
+        try {
+            long result = db.insert(TABLE_NAME, null, contentValues);
+            if(result>0){
+                return true;
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return  false;
+
+    }
+}
